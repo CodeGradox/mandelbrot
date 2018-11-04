56b076aee82ab53cf8838e1afc9ccf5208e34d22
Magnus Åsrud <magnusasrud92@gmail.com>
2018-11-04 18:38:42 +0100

    Ran cargo fmt and clippy. Added some comments.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-extern crate num_complex;
 extern crate image;
+extern crate num_complex;
 extern crate rayon;
 
 use num_complex::Complex;
 use rayon::prelude::*;
 
 fn main() {
-    mandelbrot2();
-}
-
-#[inline]
-fn mandelbrot2() {
     let max_iterations = 1000;
     let range_x = (-2.1, 0.5); // (-2.1, 0.5);
     let range_y = (-1.0, 1.0); // (-1.0, 1.0);
@@ -21,31 +16,38 @@ fn mandelbrot2() {
 
     let mut pixels = vec![0u8; (width * height) as usize];
 
-    pixels.par_iter_mut()
-        .enumerate()
-        .for_each(|(idx, n)| {
-            let x = idx % width;
-            let y = idx / height;
+    pixels.par_iter_mut().enumerate().for_each(|(idx, pixel)| {
+        // Translate 1D array position to a (x, y) coordinate in a 2D array.
+        let x = idx % width;
+        let y = idx / height;
 
-            let cx = range_x.0 + x as f32 * scale_x;
-            let cy = range_y.0 + y as f32 * scale_y;
+        // Translate the pixel position to a point in the mandelbrot set.
+        let cx = range_x.0 + x as f32 * scale_x;
+        let cy = range_y.0 + y as f32 * scale_y;
 
-            let c = Complex::new(cx, cy);
-            let mut z = Complex::new(0.0, 0.0);
+        let c = Complex::new(cx, cy);
+        let mut z = Complex::new(0.0, 0.0);
 
-            let mut i = 0;
-            for t in 0..max_iterations {
-                if z.norm_sqr() > 4.0 {
-                    break;
-                }
-                z = z * z + c;
-                i = t;
+        // Calculate z^2 + c.
+        let mut iter = 0;
+        while iter < max_iterations {
+            if z.norm_sqr() > 4.0 {
+                break;
             }
+            z = z * z + c;
+            iter += 1;
+        }
 
-            *n = -i as u8;
-        });
+        *pixel = -iter as u8;
+    });
 
     let fout = std::fs::File::create("fractal.png").expect("Could not create outputfile");
     let encoder = image::png::PNGEncoder::new(fout);
-    encoder.encode(&pixels, width as u32, height as u32, image::ColorType::Gray(8)).expect("Failed to encoede image");
+    encoder
+        .encode(
+            &pixels,
+            width as u32,
+            height as u32,
+            image::ColorType::Gray(8),
+        ).expect("Failed to encoede image");
 }
